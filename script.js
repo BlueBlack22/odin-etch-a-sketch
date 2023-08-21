499a20517432d57e1b8511e248a3454bae15a688
BlueBlack22 <115193822+BlueBlack22@users.noreply.github.com>
2023-08-21 17:58:20 +0200

    Added grid color change when mouse is hovering.

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
-const sketchpad = document.querySelector('.sketchpad');
-
-const gridDensity = 16;
-
-function createGrid(gridDensity) {
+function createGrid(gridDensity = 16) {
     const borderWidth = 1;
     gridSize = 640 / gridDensity - 2 * borderWidth;
+
+    const sketchpad = document.querySelector('.sketchpad');
+    sketchpad.innerHTML = "";
 
     for (i = 1; i <= gridDensity * gridDensity; i++) {
         const gridbox = document.createElement('div');
         gridbox.classList.add('gridbox');
         gridbox.style.cssText = `width: ${gridSize}px; height: ${gridSize}px`;
+        gridbox.addEventListener('mouseover', () => {
+            gridbox.style.backgroundColor = 'black';
+        });
         sketchpad.appendChild(gridbox);
     }
 }
 
-createGrid(gridDensity);
+createGrid();
